409251134f019ba34c262a14c1aa856187c6e34d
Larry Heimann (Prof. H) <profh@cmu.edu>
2025-02-02 19:49:30 -0500

    Created the context for faculties to speed up the exercise

--- test/sets/faculties.rb
+++ test/sets/faculties.rb
@@ -2,11 +2,15 @@ module Contexts
   module Faculties
 
     def create_faculties
-
+      @profq  = FactoryBot.create(:faculty, department: @is)
+      @koz    = FactoryBot.create(:faculty, department: @cs, first_name: "David", last_name: "Kosbie")
+      @kaufer = FactoryBot.create(:faculty, department: @english, first_name: "David", last_name: "Kaufer", active: false)
     end
     
     def destroy_faculties
-
+      @profq.destroy
+      @koz.destroy
+      @kaufer.destroy
     end
   end
 end
