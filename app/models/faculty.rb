bbd6d7a575f14d3b6624bfd02844657a7a51a831
Ted Gershon <tedgershon@gmail.com>
2025-02-04 23:22:14 -0500

    yes I should have committed incrementally. Oops

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -6,10 +6,16 @@ class Faculty < ApplicationRecord
   
     # Scopes
     # 1. `alphabetical` [order]
+    scope :active, -> { where(active: true) }
     # 2. `active`       [where]
+    scope :alphabetical, -> { order(:last_name, :first_name) }
   
     # Validations
     # 1. must have first, last names
+    validates_presence_of(:first_name)
+    validates_presence_of(:last_name)
     # 2. rank must be either `Assistant Professor`, `Associate Professor`, or `Professor`
+    # got help from copilot for the below:
+    validates :rank, inclusion: { in: ["Assistant Professor", "Associate Professor", "Professor"] }
   
 end
--- test/models/faculty_test.rb
+++ test/models/faculty_test.rb
@@ -2,7 +2,19 @@ require "test_helper"
 
 class FacultyTest < ActiveSupport::TestCase
   # Matchers
-
+  should have_many(:assignments)
+  should have_many(:courses).through(:assignments)
+  should belong_to(:department)
+  ### ensure faculty can't be created without necessary attributes:
+  # t.string :first_name
+  # t.string :last_name
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+  ### Faculty.rank = `Assistant Professor`, `Associate Professor`, or `Professor`
+  # t.string :rank
+  # copilot wrote the line below
+  should validate_inclusion_of(:rank).in_array(["Assistant Professor", "Associate Professor", "Professor"])
+  # t.boolean :active
 
 
   # Context
@@ -14,8 +26,29 @@ class FacultyTest < ActiveSupport::TestCase
     end
 
     # with that testing context, write your tests...
+    should "test first_name validation" do
+      faculty = Faculty.new(last_name: "Kim", rank: "Assistant Professor", department: @cs)
+      assert_not faculty.valid?, "Faculty should be invalid without a first name"
+      assert_includes faculty.errors[:first_name], "can't be blank"
+    end
 
-    
+    should "test last_name validation" do
+      faculty = Faculty.new(first_name: "Jisoo", rank: "Associate Professor", department: @cs)
+      assert_not faculty.valid?, "Faculty should be invalid without a last name"
+      assert_includes faculty.errors[:last_name], "can't be blank"
+    end
 
+    should "only allow valid faculty ranks" do
+      valid_ranks = ["Assistant Professor", "Associate Professor", "Professor"]
+      valid_ranks.each do |rank|
+        faculty = Faculty.new(first_name: "John", last_name: "Doe", rank: rank, department: @cs)
+        assert faculty.valid?, "Faculty with rank #{rank} should be valid"
+      end
+    end
+
+      # teardown do
+      #   destroy_departments
+      #   destroy_faculties
+      # end
   end
 end
